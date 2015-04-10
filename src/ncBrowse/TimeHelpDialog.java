825e2e38c22e2349d3159c84c80b886f921b29f8
MaxwellM <Maxwellmorin1>
2015-04-10 17:28:15 -0400

    Wow, big update!
    - When plotting from the Variable Map Dialog it will plot each, evenly into
      1/8 windows given the screen realestate (from one window to multiwindow setup).
    - Added functionality to have multiple Movie players run simultaneously !

--- src/ncBrowse/TimeHelpDialog.java
+++ src/ncBrowse/TimeHelpDialog.java
@@ -10,7 +10,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 /**
- * Provides help for setting formats for parsing and 
+ * Provides help for setting loci.formats for parsing and
  * printing time/date strings.
  *
  * @author Donald Denbo
